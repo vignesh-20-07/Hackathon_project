66fd082f76e5640119fa3f2958dd02e34085710a
vignesh-20-07 <Vignesh.M211445@cognizant.com>
2025-05-30 18:28:00 +0530

    fitness sub-menu data logging

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,22 +1,35 @@
 import carWashService from "../support/pages/carWashService";
 import fitnessSubmenu from "../support/pages/fitnessSubmenu";
- 
+
 describe("Hackathon", () => {
   Cypress.on("uncaught:exception", (err, runnable) => {
-   
+
     return false;
   });
- 
+
   it("First trial", () => {
     // Visit homepage with geolocation stubbed
     carWashService.visitHomePage(11.097208, 76.990016);
- 
+
     // carWashService.clickMaybeLater();
-    
+
   });
 
+});
+
+describe("Gym Sub-menu", () => {
+  it("First trial", () => {
+    // Visit homepage with geolocation stubbed
+    carWashService.visitHomePage(11.097208, 76.990016);
+
+    // carWashService.clickMaybeLater();
+
+  });
   it('Gym Navigation', () => {
     fitnessSubmenu.gymNavigation();
+  });
+
+  it('Gym sub-array items', () => {
+    fitnessSubmenu.subItemsFetching();
   })
-});
- 
+})
--- cypress/support/pages/fitnessSubMenu.js
+++ cypress/support/pages/fitnessSubMenu.js
@@ -1,7 +1,22 @@
 class fitnessSubMenu {
     gymNavigation() {
-        cy.get('[href="/Coimbatore/Gyms-in-Chil-Sez-Road-Saravanampatti/nct-11575244"] > .home_hotkeyimg').click();
+        cy.get('[href="/Coimbatore/Gyms-in-Chil-Sez-Road-Saravanampatti/nct-11575244"] > .home_hotkeyimg')
+            .click();
     }
+
+    subItemsFetching() {
+        const submenu = [];
+        cy.get('.jsx-8e2185bd5f884df4 .font15')
+            .each(($el) => {
+                cy.wrap($el).invoke('text').then((text) => {
+                    submenu.push(text);
+                });
+            });
+        cy.wrap(submenu).each((data) => {
+            cy.log(data);
+        })
+    }
+
 }
 
 export default new fitnessSubMenu();
