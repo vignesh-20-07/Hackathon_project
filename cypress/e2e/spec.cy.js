5e2fb8e8c498fdf38821c065135a016cb48a1691
Sanmathi3008 <Sanmathi.MK@cognizant.com>
2025-06-02 11:56:22 +0530

    Added free listing navigation check

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,9 +18,10 @@ describe('Car Wash Service Automation - Free Listing Registration', () => {
     return false;
   });
 
-  it('Free Listing Navigation', function () {
+  it('Free Listing Navigation', () => {
     freeListing.visit();
     freeListing.clickMaybeLater();
+
     freeListing.clickFreeListing();
     freeListing.verifyUrl();
 
