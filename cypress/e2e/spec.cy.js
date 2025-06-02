c7ba3b71e2f4cb4b4ff5876d674403a78f5a17fe
Sanmathi3008 <Sanmathi.MK@cognizant.com>
2025-06-02 19:44:01 +0530

    Added validation for phone number length

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,17 @@
-import carWashService from "../support/pages/carWashService";
+// import carWashService from "../support/pages/carWashService";
 import freeListing from "../support/pages/freeListing";
 
-describe("Hackathon", () => {
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    return false;
-  });
+// describe("Hackathon", () => {
+//   Cypress.on("uncaught:exception", (err, runnable) => {
+//     return false;
+//   });
 
-  it("First trial", () => {
-    carWashService.visitHomePage(11.097208, 76.990016);
-    carWashService.clickMaybeLater();
+//   it("First trial", () => {
+//     carWashService.visitHomePage(11.097208, 76.990016);
+//     carWashService.clickMaybeLater();
     
-  });
-});
+//   });
+// });
 
 describe('Free Listing Registration', () => {
   Cypress.on("uncaught:exception", (err, runnable) => {
--- cypress/support/pages/carWashService.js
+++ cypress/support/pages/carWashService.js
@@ -1,22 +1,22 @@
-class CarWashService {
-    visitHomePage(latitude, longitude) {
-      cy.visit("https://www.justdial.com/", {
-        onBeforeLoad({ navigator }) {
-          cy.stub(navigator.geolocation, "getCurrentPosition")
-            .callsArgWith(0, { coords: { latitude, longitude } });
-        },
-      });
+// class CarWashService {
+//     visitHomePage(latitude, longitude) {
+//       cy.visit("https://www.justdial.com/", {
+//         onBeforeLoad({ navigator }) {
+//           cy.stub(navigator.geolocation, "getCurrentPosition")
+//             .callsArgWith(0, { coords: { latitude, longitude } });
+//         },
+//       });
    
-      cy.wait(5000);
-    }
+//       cy.wait(5000);
+//     }
    
-    clickMaybeLater() {
-      cy.get('.maybelater > .jsx-c21ded63fbf3c5d8')
-        .should("be.visible")
-        .click();
-    }
+//     clickMaybeLater() {
+//       cy.get('.maybelater > .jsx-c21ded63fbf3c5d8')
+//         .should("be.visible")
+//         .click();
+//     }
    
     
-  }
+//   }
    
-  export default new CarWashService();
+//   export default new CarWashService();
