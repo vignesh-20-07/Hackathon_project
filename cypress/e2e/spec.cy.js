dfaca6dd0eb1b57237bdb7e424bbdbb46d867206
Sanmathi3008 <Sanmathi.MK@cognizant.com>
2025-06-02 16:38:37 +0530

    Added free listing navigation check

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,12 +18,5 @@ describe('Free Listing Registration', () => {
     return false;
   });
 
-  it('Free Listing Navigation', () => {
-    freeListing.visit();
-    freeListing.clickMaybeLater();
 
-    freeListing.clickFreeListing();
-    freeListing.verifyUrl();
-
-  });
 });
