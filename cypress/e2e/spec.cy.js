6f496cf8fd7323f81bec55e046f4ef1af2358b19
Rachana87 <N.Rachana@cognizant.com>
2025-06-03 09:23:57 +0530

    Small updates in development

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,12 +2,12 @@ import carWashService from "../support/pages/carWashService";
 import fitnessSubmenu from "../support/pages/fitnessSubmenu";
 import freeListing from "../support/pages/freeListing";
 
-describe.only("Hackathon", () => {
+describe.only("Car Wash Service searching and sorting", () => {
   Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
   });
 
-  it("First trial", () => {
+  it("Visiting the site", () => {
     carWashService.visitHomePage(11.097208, 76.990016);
     carWashService.clickMaybeLater();
     
